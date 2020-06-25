fbd0e972d95df086c8c7cac07224105f1796a502
RazzSunuwar <razzkoinch5@gmail.com>
2020-06-25 05:58:04 +0545

    Modified the codes

--- main.ts
+++ main.ts
@@ -1,29 +1,15 @@
-// function doSomething(){
-//     for (var i = 0; i< 5; i++) {
-//         console.log(i);
-//     }
-//     console.log('Finally ' + i);
-// }
-// doSomething();
+function doSomething(){
+    for (var i = 0; i< 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally ' + i);
+}
+doSomething();
 
-// /*OUTPUT
-// 0
-// 1
-// 2
-// 3
-// 4
-// Finally: 5*/
-
-let a: number;
-let b: number;
-let c: number;
-let d: number;
-let e: number;
-let f: any[] = [1, true, 'a', false];
-
-const ColorRed = 0;
-const ColorGreen = 1;
-const ColorBlue = 2;
-
-enum Color {Red = 0, Green = 1, Purple = 2, Blue = 3 };
-let backgroundColor = Color.Red;
+/*OUTPUT
+0
+1
+2
+3
+4
+Finally: 5*/
